4060e0f605a624d78dcd28fdf0d301cc4228ca18
Peter Munachi <alimunachipeter@gmail.com>
2024-05-11 13:21:14 +0100

    feat: adding env example file

--- src/modules/orderBook/order.module.ts
+++ src/modules/orderBook/order.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
-import { HttpModule } from '@nestjs/axios';
 import { PrismaService } from 'src/services/prisma.service';
 
 @Module({
-  imports: [
-    HttpModule.register({
-      baseURL: process.env.MODULUS_URL,
-    }),
-  ],
+  imports: [],
   controllers: [OrderController],
   providers: [OrderService, PrismaService],
 })
